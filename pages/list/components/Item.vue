14857ae48ec06224e55e998196b6f742d4acb917
zhangchunhui <zhangchunhui@yunzhonghe.com>
2024-11-27 11:58:16 +0800

    图片展示

--- pages/list/components/Item.vue
+++ pages/list/components/Item.vue
@@ -16,7 +16,7 @@
           <div class="flex mb-[--gap] rounded-md overflow-hidden transition-all translate-y-[-5px] hover:drop-shadow-xl shadow-xl dark:bg-black border border-[--el-border-color]">
             <el-image
               :class="{ order: index % 2 === 0 ? 2 : 1 }"
-              :src="data.imgUrl || data.content?getImgUrl(data.content):''"
+              :src="data.img || data.imgUrl || (data.content?getImgUrl(data.content):'')"
               fit="cover"
               class="w-[120px] h-[115px] md:w-[240px] md:h-[160px]" />
             <div class="flex-1 ml-[--gap] p-4 max-w-[60%] md:max-w-[73%] md:max-h-[160px]" :class="{ order: index % 2 === 0 ? 1 : 2 }">
