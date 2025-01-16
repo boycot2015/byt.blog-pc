356146ad79920249a28041d8a7e7ce7df5ca9492
zhangchunhui <boycot2017@163.com>
2025-01-16 18:50:00 +0800

    样式

--- components/Editor/index.vue
+++ components/Editor/index.vue
@@ -30,8 +30,8 @@
 
 <script lang="ts" setup>
 import { MdEditor, MdPreview, MdCatalog } from 'md-editor-v3'
-// import 'md-editor-v3/lib/style.css'
-// import 'md-editor-v3/lib/preview.css'
+import 'md-editor-v3/lib/style.css'
+import 'md-editor-v3/lib/preview.css'
 import { useDark } from '@vueuse/core'
 
 interface Props {
